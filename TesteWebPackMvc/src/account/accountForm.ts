cdc5895c64b99d0e8fc519cc6b3a4e51278845ec
Fabio Salomao <fabio.silva@7comm.com.br>
2023-08-15 08:48:03 -0500

    feat: validação do form de login

--- TesteWebPackMvc/src/account/accountForm.ts
+++ TesteWebPackMvc/src/account/accountForm.ts
@@ -7,11 +7,13 @@ const usernameInput = document.getElementById('txtUsername') as HTMLInputElement
 const btnLogin = document.getElementById('btnLogin') as HTMLButtonElement;
 const formLogin = document.getElementById('formLogin') as HTMLFormElement;
 const preResult = document.getElementById('preResult') as HTMLPreElement;
+const emailValidation = document.getElementById('emailValidation') as HTMLDivElement;
+const usernameValidation = document.getElementById('usernameValidation') as HTMLDivElement;
 
 
 const UserSchema = z.object({
     username: z.string().nonempty('Username é obrigatório'),
-    email: z.string().email('Formato de e-mail inválido').nonempty('E-mail é obrigatório'),
+    email: z.string().nonempty('E-mail é obrigatório').email('Formato de e-mail inválido'),
 });
 
 type UserFormData = z.infer<typeof UserSchema>;
@@ -32,10 +34,15 @@ const login = async () => {
 
     const result = UserSchema.safeParse(formData);
     if (!result.success) {
-        alert('Erro no campo: ' + result.error.errors[0].path + '. Tipo de erro: ' + result.error.errors[0].message);
+        const erroFormat = result.error.format();
+        emailValidation.textContent = erroFormat.email?._errors.join(",") || "";
+        usernameValidation.textContent = erroFormat.username?._errors.join(",") || "";
+
     } else {
         const user = await getUser(formData.email, formData.username);
         preResult.innerHTML = JSON.stringify(user, null, 2);
+        emailValidation.textContent = "";
+        usernameValidation.textContent = "";
     } 
 
     btnLogin.removeAttribute('disabled');
